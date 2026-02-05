65475e003613e01aa041b217eee516bb2072adaa
Nicolò Verardo <n.verardo@outlook.com>
2026-02-06 00:25:37 +0100

    feat: added Sync and Async clients

---
+++ src/maws/client.py
@@ -0,0 +1,138 @@
+import asyncio
+import logging
+import math
+from pathlib import Path
+
+import httpx
+from pydantic import TypeAdapter
+
+from maws.config import Config
+from maws.models import Product
+from maws.parser import ProductHTMLParser
+
+logger = logging.getLogger(__name__)
+
+
+class BaseClient:
+    def __init__(self, config: Config | None = None):
+        self.config = config or Config()
+
+    def _parse_products_from_html(self, data: httpx.Response | str) -> list[Product]:
+        html = data.text if isinstance(data, httpx.Response) else data
+
+        parser = ProductHTMLParser()
+        parser.feed(html)
+        products = [Product(**p) for p in parser.products]
+        return products
+
+    def parse_folder(
+        self, folder: str | Path, output_json: str | Path | None = None
+    ) -> list[Product]:
+        folder = Path(folder)
+        all_products: list[Product] = []
+
+        for file in folder.glob("page_*.html"):
+            logger.info("Parsing products from '%s'…", file)
+            html = file.read_text(encoding="utf-8")
+            products = self._parse_products_from_html(html)
+            all_products.extend(products)
+
+        logger.info("Total products parsed: %d", len(all_products))
+
+        if output_json is not None:
+            output_json = Path(output_json)
+            output_json.parent.mkdir(exist_ok=True, parents=True)
+            logger.info("Saving parsed products to '%s'…", output_json)
+            Path(output_json).write_bytes(
+                TypeAdapter(list[Product]).dump_json(all_products)
+            )
+
+        return all_products
+
+
+class SyncClient(BaseClient):
+    def __init__(self, config: Config | None = None):
+        self.client = httpx.Client(follow_redirects=True)
+        super().__init__(config)
+
+    def fetch_first_product_page(self) -> httpx.Response:
+        resp = self.client.get(
+            self.config.urls.products_url,
+            headers=self.config.user_agents.get_random_ua_header(),
+        )
+        resp.raise_for_status()
+
+        return resp
+
+    def close(self):
+        self.client.close()
+
+
+class AsyncClient(BaseClient):
+    def __init__(self, config: Config | None = None):
+        self.rate_limiter = asyncio.Semaphore(1)
+        super().__init__(config)
+
+    async def fetch(self, client: httpx.AsyncClient, url: str) -> httpx.Response:
+        async with self.rate_limiter:
+            response = await client.get(
+                url, headers=self.config.user_agents.get_random_ua_header()
+            )
+            response.raise_for_status()
+            await asyncio.sleep(self.config.urls.rate_limit_seconds)
+            return response
+
+    async def download_all_pages(
+        self, output: str | Path = "output", max_pages: int | None = None
+    ) -> Path:
+        Path(output).mkdir(exist_ok=True, parents=True)
+
+        async with httpx.AsyncClient(timeout=self.config.urls.timeout) as client:
+            # Fetch first page
+            logger.info("Fetching first page to determine total count…")
+
+            first_html = await self.fetch(client, self.config.urls.products_url)
+
+            total_products = ProductHTMLParser().extract_total_products(first_html.text)
+            got_pages = math.ceil(
+                total_products / int(self.config.urls.list_limit_value)
+            )
+            total_pages: int = (
+                min(got_pages, max_pages) if max_pages is not None else got_pages
+            )
+            logger.info(
+                "Total products: %d → Total pages: %d (max pages: %s)",
+                total_products,
+                got_pages,
+                max_pages,
+            )
+
+            # Save first page
+            first_path = Path(output, "page_1.html")
+            first_path.write_text(first_html.text, encoding="utf-8")
+
+            # URLs for remaining pages
+            urls: list[tuple[int, str]] = [
+                (
+                    page,
+                    f"{self.config.urls.products_url}&p={page}",
+                )
+                for page in range(2, total_pages + 1)
+            ]
+
+            async def wrapped_fetch(page_num: int, url: str):
+                html = await self.fetch(client, url)
+
+                logger.info("Page %d fetched, saving to disk…", page_num)
+
+                path = Path(output, f"page_{page_num}.html")
+                path.write_text(html.text, encoding="utf-8")
+
+                return path
+
+            # Parallel download
+            await asyncio.gather(*[wrapped_fetch(page, url) for page, url in urls])
+
+            logger.info("All %d pages saved to '%s'", total_pages, output)
+
+        return Path(output)
