6d99b553a2b123707fbc3435eb1e3d8c6e2b4642
Nicolò Verardo <n.verardo@outlook.com>
2026-02-19 12:45:37 +0100

    feat: cli functions

--- src/cli/main.py
+++ src/cli/main.py
@@ -3,18 +3,24 @@ import datetime
 import logging
 from pathlib import Path
 from typing import Annotated, Optional
+from uuid import uuid4
 
 import typer
 from rich.logging import RichHandler
 
 from maws import MawsAsyncClient
+from maws.playwright import main
 
 app = typer.Typer(no_args_is_help=True)
 
+logging_format = "%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s"
 logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s",
-    handlers=[RichHandler()],
+    level=logging.DEBUG,
+    format=logging_format,
+    handlers=[
+        RichHandler(level=logging.INFO),
+        logging.FileHandler("maws.log", encoding="utf-8"),
+    ],
 )
 
 
@@ -29,7 +35,7 @@ products_app = typer.Typer(no_args_is_help=True)
 
 
 @products_app.command()
-def download(
+def download_list(
     output: Annotated[
         Path,
         typer.Option(
@@ -56,6 +62,42 @@ def download(
 
 
 @products_app.command()
+def download_details(
+    products: Annotated[
+        Path,
+        typer.Option(
+            help="Path to the JSON file containing product data.",
+            file_okay=True,
+            dir_okay=False,
+            readable=True,
+            exists=True,
+        ),
+    ],
+    output: Annotated[
+        Optional[Path],
+        typer.Option(
+            help="Where to save the downloaded pages",
+            file_okay=False,
+            dir_okay=True,
+            writable=True,
+        ),
+    ] = None,
+    timeout: Annotated[
+        int,
+        typer.Option(
+            help="Timeout for loading each product page, in milliseconds.",
+            min=1,
+        ),
+    ] = 30000,
+):
+    """Download Products details page."""
+    if output is None:
+        output = Path("output", str(uuid4()))
+    output.mkdir(parents=True, exist_ok=True)
+    asyncio.run(main(products=products, output=output, timeout=timeout))
+
+
+@products_app.command()
 def parse(
     source: Annotated[
         Path,
@@ -76,11 +118,37 @@ def parse(
             writable=True,
         ),
     ],
+    is_details: Annotated[
+        bool,
+        typer.Option(
+            "--is-details",
+            help="Whether the pages contain detailed product information.",
+            is_flag=True,
+        ),
+    ] = False,
+    input_json: Annotated[
+        Optional[Path],
+        typer.Option(
+            help="Path to the input JSON file containing product data. Only used when --is-details is passed.",
+            file_okay=True,
+            dir_okay=False,
+            readable=True,
+            exists=True,
+        ),
+    ] = None,
 ):
     """Parse a folder containing HTML downloaded pages and save them to JSON."""
     client = MawsAsyncClient()
-    file_name = datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + "_products.json"
-    _ = client.parse_folder(folder=source, output_json=Path(output, file_name))
+    name_part = "products_detailed" if is_details else "products"
+    file_name = (
+        datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + "_" + name_part + ".json"
+    )
+    if is_details:
+        _ = client.parse_detailed_product_folder(
+            folder=source, input_json=input_json, output_json=Path(output, file_name)
+        )
+    else:
+        _ = client.parse_folder(folder=source, output_json=Path(output, file_name))
 
 
 app.add_typer(products_app, name="products", help="Commands related to Products.")
