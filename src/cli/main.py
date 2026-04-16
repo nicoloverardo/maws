1fa991184f6a8fb506df5a8252b5188530a0220e
Nicolò Verardo <n.verardo@outlook.com>
2026-04-16 23:23:28 +0200

    feat: implementing first draft of asia 4 friends

--- src/cli/main.py
+++ src/cli/main.py
@@ -3,6 +3,7 @@ import logging
 import typer
 from rich.logging import RichHandler
 
+from .a4f import a4f
 from .aof import aof
 from .goa import goa
 
@@ -28,3 +29,4 @@ def callback():
 
 app.add_typer(aof, name="aof", help="Commands related to Asia Express Food.")
 app.add_typer(goa, name="goa", help="Commands related to Go Asia.")
+app.add_typer(a4f, name="a4f", help="Commands related to Asia 4 Friends.")
--- src/maws/__init__.py
+++ src/maws/__init__.py
@@ -1,4 +1,5 @@
+from maws.a4f.client import JTLScraper
 from maws.aof.client import MawsAsyncClient
 from maws.goa.client import GoaAsyncClient
 
-__all__ = ["MawsAsyncClient", "GoaAsyncClient"]
+__all__ = ["MawsAsyncClient", "GoaAsyncClient", "JTLScraper"]
